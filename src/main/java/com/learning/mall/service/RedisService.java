e9e782b4215f005cc4fe76cb7d5a2c07d73814c5
Money8888 <13516139951@163.com>
2019-11-29 17:31:13 +0800

    redis 练习 第二天

--- src/main/java/com/learning/mall/service/RedisService.java
+++ src/main/java/com/learning/mall/service/RedisService.java
@@ -10,7 +10,7 @@ public interface RedisService {
     void set(String key, String value);
     // 获取数据
     String get(String key);
-    // 设置超时时间
+    // 设置验证码过期时间
     boolean expire(String key, long expire);
     // 删除数据
     void remove(String key);
